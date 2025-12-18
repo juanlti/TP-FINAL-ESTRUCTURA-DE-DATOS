98d61e04c2cd2a514d969ed5391d43eabfefa160
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2025-12-18 13:54:11 -0300

    fix

--- Linea.java
+++ Linea.java
@@ -12,9 +12,9 @@ package trenes;
 public class Linea {
 
     private String nombre;
-    private String[] estaciones;
+    private Lista estaciones;
 
-    public  Linea(String nombre, String[] estaciones) {
+    public Linea(String nombre, Lista estaciones) {
         this.nombre = nombre;
         this.estaciones = estaciones;
     }
@@ -27,21 +27,23 @@ public class Linea {
         this.nombre = nombre;
     }
 
-    public String[] getEstaciones() {
+    public Lista getEstaciones() {
         return estaciones;
     }
 
-    public void setEstaciones(String[] estaciones) {
+    public void setEstaciones(Lista estaciones) {
         this.estaciones = estaciones;
     }
 
     @Override
     public String toString() {
         String estaciones = "";
-        for (int i = 0; i < this.estaciones.length; i++) {
-            estaciones = this.estaciones[i] + " , " + estaciones;
+        for (int i = 1; i < this.estaciones.longitud(); i++) {
+            estaciones = this.estaciones.recuperar(i) + estaciones;
         }
+
         return "Linea : " + nombre + "estaciones [ " + estaciones + " ]";
+
     }
 
 }
